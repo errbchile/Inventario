55e3935dd23515ca96d6f36d6334e41d227e3580
eromero <eromero@inteduc.cl>
2021-02-08 21:22:46 -0300

    Pasamos de VUE 2 a VUE 3

--- app.js
+++ app.js
@@ -1,153 +1,56 @@
-Vue.component('TarjetaDeAtributo', {
-   template: `
-   <div>
-      <div class="card m-2" style="width: 18rem;">
-         <div class="card-body">
-         <h5 class="card-title">
-            <div class="row" v-if="!atributoEnEdicion">
-               <div class="col">
-                  <strong>{{ atributo.nombre }}</strong>
-               </div>
-               <div class="col text-right">
-                  <a @click.prevent="atributoEnEdicion = !atributoEnEdicion" href="#" class="btn btn-outline-info btn-sm"><i class="fas fa-edit"></i></a>
-                  <a @click.prevent="removeAtributo(atributo.nombre)" href="#" class="btn btn-outline-danger btn-sm"><i class="fas fa-times-circle"></i></a>
-               </div>
-            </div>
+const {createApp} = Vue;
 
-            <div class="row" v-else-if="atributoEnEdicion">
-               <div class="col">
-                  <form>
-                     <div class="form-group text-center">
-                     <div class="input-group mb-2">
-                        <input v-model="atributo.nombre" type="text" class="form-control">
-                        <div class="input-group-append">
-                           <button @click.prevent="atributoEnEdicion = !atributoEnEdicion" class="input-group-text btn btn-outline-danger"><i class="fas fa-times-circle"></i></button>
-                        </div>
-                     </div>
-                     </div>
-                  </form>
-               </div>
-            </div>
-
-         </h5>
-         </div>
-         <ul class="list-group list-group-flush">
-            <li class="list-group-item" v-for="(valor, pos) in atributo.valores" :key="pos">
-               <div class="row">
-                  <div class="col">
-                  {{ valor }}
-                  </div>
-                  <div class="col">
-                     <a href="#" class="btn btn-outline-info btn-sm"><i class="fas fa-edit"></i></a>
-                     <a @click.prevent="removeValue(valor, atributo.nombre)" href="#" class="btn btn-outline-danger btn-sm"><i class="fas fa-times-circle"></i></a>
-                  </div>
-               </div>
-            </li>
-            <li class="list-group-item">
-               <form>
-                  <div class="form-group text-center">
-                  <div class="input-group mb-2">
-                     <input v-model="newValor" type="text" class="form-control" placeholder="Nuevo Valor...">
-                     <div class="input-group-append">
-                        <button @click.prevent="handleNewValor(atributo.nombre)" class="input-group-text btn btn-outline-success"><i class="fa fa-plus"></i></button>
-                     </div>
-                  </div>
-                  </div>
-               </form>
-            </li>
-         </ul>
-      </div>
-   </div>
-   `,
-   data(){
-      return {
-         atributoEnEdicion: false,
-         newAtributo: "",
-         newValor: ""
-      }
-   },
-
-   props: ['atributo', 'removeAtributo'],
-
-   methods: {
-      handleNewValor(atributoNombre){
-         if (this.newValor === "") {
-            return;
-         }
-
-         let obj = {
-            atributoNombre,
-            newValor: this.newValor
-         }
-         this.$emit('new-valor', obj);
-         this.newValor = "";
-      },
-
-      removeValue(value, atributo){
-         let obj = {
-            value,
-            atributo
-         }
-         this.$emit('remove-valor', obj);
-      }
-   } // end Methods
-})
-
-
-
-
-
-
-
-
-const appVue = new Vue({
+const application = createApp({
    el: '#appVue',
-   data: {
-      registroDePlantillas: [
-         {
-            nombreProducto: 'Pestaña',
-            atributos: [
-               {
-                  nombre: 'Medida',
-                  valores: ['0.05', '0.07', '0.20']
-               },
-               {
-                  nombre: 'Color',
-                  valores: ['Rojo', 'Negro', 'Transparente']
-               }
-            ],
-         }
-      ],
+   data() {
+      return {
 
-      buscarNombreProducto: "",
-
-      newProducto: {
-         nombreProducto: '',
-         atributos: []
-      },
-
-      newAtributo: {
-         nombre: '',
-         valores: []
-      },
-
-      atributoEnModoEdicion: false,
-
-      productoEnRegistroDePlantillas: false,
-      nombreDeProductosRegistrados: [],
-      agregarNuevoNombreDeProducto: false,
-      agregarNombreDeProductoExistente: false,
-      atributoNoValidoParaSerAgregado: false,
-
-      formCompra: {
-         productoSeleccionadoParaComprar: "",
-         cantidadItemsEnCompra: null,
-         montoTotalPagado: null,
-         costoPorUnidad: null,
-         atributos: {}
-      },
-
-      comprasRegistradas: [],
+         registroDePlantillas: [
+            {
+               nombreProducto: 'Pestaña',
+               atributos: [
+                  {
+                     nombre: 'Medida',
+                     valores: ['0.05', '0.07', '0.20']
+                  },
+                  {
+                     nombre: 'Color',
+                     valores: ['Rojo', 'Negro', 'Transparente']
+                  }
+               ],
+            }
+         ],
+   
+         buscarNombreProducto: "",
+   
+         newProducto: {
+            nombreProducto: '',
+            atributos: []
+         },
+   
+         newAtributo: {
+            nombre: '',
+            valores: []
+         },
+   
+         atributoEnModoEdicion: false,
+   
+         productoEnRegistroDePlantillas: false,
+         nombreDeProductosRegistrados: [],
+         agregarNuevoNombreDeProducto: false,
+         agregarNombreDeProductoExistente: false,
+         atributoNoValidoParaSerAgregado: false,
+   
+         formCompra: {
+            productoSeleccionadoParaComprar: "",
+            cantidadItemsEnCompra: null,
+            montoTotalPagado: null,
+            costoPorUnidad: null,
+            atributos: {}
+         },
+   
+         comprasRegistradas: [],
+      }
 
    },
 
@@ -312,3 +215,106 @@ const appVue = new Vue({
    }
 
 })
+
+
+
+
+
+
+application.component('TarjetaDeAtributo', {
+   template: `
+   <div>
+      <div class="card m-2" style="width: 18rem;">
+         <div class="card-body">
+         <h5 class="card-title">
+            <div class="row" v-if="!atributoEnEdicion">
+               <div class="col">
+                  <strong>{{ atributo.nombre }}</strong>
+               </div>
+               <div class="col text-right">
+                  <a @click.prevent="atributoEnEdicion = !atributoEnEdicion" href="#" class="btn btn-outline-info btn-sm"><i class="fas fa-edit"></i></a>
+                  <a @click.prevent="removeAtributo(atributo.nombre)" href="#" class="btn btn-outline-danger btn-sm"><i class="fas fa-times-circle"></i></a>
+               </div>
+            </div>
+
+            <div class="row" v-else-if="atributoEnEdicion">
+               <div class="col">
+                  <form>
+                     <div class="form-group text-center">
+                     <div class="input-group mb-2">
+                        <input v-model="atributo.nombre" type="text" class="form-control">
+                        <div class="input-group-append">
+                           <button @click.prevent="atributoEnEdicion = !atributoEnEdicion" class="input-group-text btn btn-outline-danger"><i class="fas fa-times-circle"></i></button>
+                        </div>
+                     </div>
+                     </div>
+                  </form>
+               </div>
+            </div>
+
+         </h5>
+         </div>
+         <ul class="list-group list-group-flush">
+            <li class="list-group-item" v-for="(valor, pos) in atributo.valores" :key="pos">
+               <div class="row">
+                  <div class="col">
+                  {{ valor }}
+                  </div>
+                  <div class="col">
+                     <a href="#" class="btn btn-outline-info btn-sm"><i class="fas fa-edit"></i></a>
+                     <a @click.prevent="removeValue(valor, atributo.nombre)" href="#" class="btn btn-outline-danger btn-sm"><i class="fas fa-times-circle"></i></a>
+                  </div>
+               </div>
+            </li>
+            <li class="list-group-item">
+               <form>
+                  <div class="form-group text-center">
+                  <div class="input-group mb-2">
+                     <input v-model="newValor" type="text" class="form-control" placeholder="Nuevo Valor...">
+                     <div class="input-group-append">
+                        <button @click.prevent="handleNewValor(atributo.nombre)" class="input-group-text btn btn-outline-success"><i class="fa fa-plus"></i></button>
+                     </div>
+                  </div>
+                  </div>
+               </form>
+            </li>
+         </ul>
+      </div>
+   </div>
+   `,
+   data(){
+      return {
+         atributoEnEdicion: false,
+         newAtributo: "",
+         newValor: ""
+      }
+   },
+
+   props: ['atributo', 'removeAtributo'],
+
+   methods: {
+      handleNewValor(atributoNombre){
+         if (this.newValor === "") {
+            return;
+         }
+
+         let obj = {
+            atributoNombre,
+            newValor: this.newValor
+         }
+         this.$emit('new-valor', obj);
+         this.newValor = "";
+      },
+
+      removeValue(value, atributo){
+         let obj = {
+            value,
+            atributo
+         }
+         this.$emit('remove-valor', obj);
+      }
+   } // end Methods
+})
+
+
+application.mount('#appVue')
